9a64608e3eba52ed060d0039edc371ea9925e3d0
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-08 18:08:59 -0700

    updated betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+include <stdio.h>
 /**
- * main - it prints Programming is like building a multilingual puzzle
- * return : 0 returns zero 
+ *  * main - main block
+ *   *Return: 0
  */
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
- }
+int main(void)
+{
+              puts("\"Programming is like building a multilingual puzzle");
+              return (0);
+}	      
