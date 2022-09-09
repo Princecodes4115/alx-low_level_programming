ad96687fbfb04628adeb5bedcd4455f340b760e6
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-09 09:05:00 -0700

    added task 0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - prints exactly "and that piece of art is useful"
-* - Dora Korpar, 2015-10-19",
-* followed by a new line, to the standard error.
-* Return: Always 0 (Success)
+* main -prints 
+*
+* and that piece of art is useful\n" - Dora Korpar, 2015-10-19",		
+* Return: Always 1.
 */
 int main(void)
 {
-    write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
