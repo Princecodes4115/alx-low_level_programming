5e258a82897474567520c8e58dcb1e535714bcf3
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-08 18:59:25 -0700

    added task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - prints "Programming is like building a multilingual puzzle"
 *
@@ -9,4 +10,3 @@ int main(void)
        puts("\"Programming is like building a multilingual puzzle");
        return (0);
 }
-
