32ec564f4f273d02d12103c24a82a74876eec138
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-10 06:45:35 -0700

    solved task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-* main -prints 
+* main - prints
 *
-* and that piece of art is useful\n" - Dora Korpar, 2015-10-19",		
+* and that piece of art is useful\n" - Dora Korpar, 2015-10-19" ,
+*
 * Return: Always 1.
 */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
