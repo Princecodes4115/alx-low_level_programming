da343f0b95e3982d749231825680ff530d2560a6
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-13 18:00:02 -0700

    solved task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef_HOBERTON_H_INCLUDED
-#define_HOBERTON_H_INCLUDED
+#ifndef HOBERTON_H_INCLUDED
+#define HOBERTON_H_INCLUDED
 
 int putchar(char);
 void print_alphabet(void);
@@ -14,3 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
