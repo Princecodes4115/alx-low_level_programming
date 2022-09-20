e304b81cefb13312daece5ec1000f2d1b8c60b8d
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-19 19:01:20 -0700

    added task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,4 @@
-#ifndef "main.h"
-#define "main.h"
-
+void reset_to_98(int *n);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +10,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-
-#endif
