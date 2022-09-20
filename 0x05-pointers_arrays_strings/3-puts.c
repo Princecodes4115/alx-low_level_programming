c2249595366538d6cd214259a65a4394500edca4
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-20 16:13:20 -0700

    solved task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,13 +4,15 @@
 *   @str: pointer to the string to print
 *   Return: void
 */
+
+
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+int i = 0;
+while (str[i])
+{
+	_putchar(str[i]);
+	i++;
+}
+_putchar('\n');
 }
