9e64a9499603acf28861640e9523172299f88b88
Ezekiel Moses <codewithprince.com@gmail.com>
2022-09-28 06:59:48 -0700

    solved task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 
 /**
 * _puts_recursion - prints a string, followed by a new line
-*  @s: string to print
+*  @s: pointer to string
+*
+*  Return: void
 */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	else
-	{
 	_putchar(*s);
-	_puts_recursion(++s);
-	}
+	_puts_recursion(s + 1);
 }
