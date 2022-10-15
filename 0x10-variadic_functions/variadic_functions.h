39ad485b9c60aea34ef4c267716cce0f5786ac4b
Ezekiel Moses <codewithprince.com@gmail.com>
2022-10-15 06:46:51 -0700

    corrected header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,6 @@
 #ifndef VAR_FUNC_H
 #define VAR_FUNC_H
-#include <sdarg.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
